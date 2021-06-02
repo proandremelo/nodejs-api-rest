76963169194ad0c77f518668aef6f0379d2f3f1e
André Melo <proandremelo@gmail.com>
2021-06-02 10:00:47 -0300

    Conectando com o mysql

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const customExpress = require("./config/customExpress");
+const conexao = require("./infraestrutura/conexao")
 
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro);
+    }
+    console.log("Conectou!");
+})
 const app = customExpress();
 const porta = 3000;
 
